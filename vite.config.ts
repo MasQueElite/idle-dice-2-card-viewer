459a673222ce3a5790e58daf09351b9ad9ada942
MasQueElite <68552309+MasQueElite@users.noreply.github.com>
2026-09-16 14:40:56 +0200

    oops assets are not at the root dir
    github pages moment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [solid(), tailwindcss()],
+    base: "idle-dice-2-card-viewer",
     build: {
         outDir: "docs"
     }
